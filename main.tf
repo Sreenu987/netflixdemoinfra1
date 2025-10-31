f102da5aec80b20291285d161bf1626723700642
Sreenivasula Reddy Bapathi <sreenureddybapathi@gmail.com>
2025-10-31 19:21:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ provider "aws" {
 
 resource "aws_instance" "one" {
   count                  = 4
-  ami                    = "ami-02d26659fd82cf299"
+  ami                    = "ami-09186c326c40fd6cc"
   instance_type          = "t3.small"
-  key_name               = "jenkins"
-  vpc_security_group_ids = ["sg-002dba717d31408b7"]
+  key_name               = "PROJECT 1"
+  vpc_security_group_ids = ["sg-01916c9d4f5f21716"]
   tags = {
     Name = var.instance_names[count.index]
   }
